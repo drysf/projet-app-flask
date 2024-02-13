a2d626dff4d989eb4670e3c75745d60c3c43efaf
drysf <drys.ferhi@gmail.com>
2024-02-13 14:12:09 +0100

    third commit

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
 @app.route('/')
+def indexhtml():
+    return "hello world"
+
+
+@app.route('/index')
 def index():
     return render_template('index.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
