e3f58778333238598d1791da1a8bb124d1ae0125
drysf <drys.ferhi@gmail.com>
2024-02-16 01:30:45 +0100

    quatrieme data error trop de donnees

--- app.py
+++ app.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+url = 'https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv'
+data = pd.read_csv(url)
+
+url2 = 'https://raw.githubusercontent.com/plotly/datasets/master/2014_usa_states.csv'
+data2 = pd.read_csv(url2)
+
+
+@app.route('/about')
+def hello_world():
+    return 'Hello, World'
+
+url4 = "https://www.youtube.com/watch?v=NuTAgztj_NI"
+data4 = requests.get(url)
 
 @app.route('/')
-def indexhtml():
-    return "hello world"
+def about():
+    return render_template('about.html', data=data, data2=data2, data4=data4)
 
+    
 
-@app.route('/index')
-def index():
-    return render_template('index.html')
+if __name__ == '__main__':
+    app.run(debug=True)
--- model/data.py
+++
@@ -1,9 +0,0 @@
-class MyClass:
-    def __init__(self):
-        self._data = None
-
-    def get_data(self):
-        return self._data
-
-    def set_data(self, new_data):
-        self._data = new_data
